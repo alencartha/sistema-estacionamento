a7db8a196a3e6c67a92c2235dd000dc7723987c4
alencartha <alencar_tha@hotmail.com>
2023-08-15 19:09:45 -0300

    feature: rota buscarTodos, buscarUm e cadastrarCarro

--- backend/src/controllers/CarroController.js
+++ backend/src/controllers/CarroController.js
@@ -1,1 +1,63 @@
 const CarroService = require('../services/CarroService');
+
+module.exports = {
+
+    buscarTodos: async (req, res) => {
+
+        let json = { error: '', result: [] };
+
+        let carros = await CarroService.buscarTodos();
+
+        for (let i in carros) {
+            json.result.push({
+                codigo: carros[i].codigo,
+                modelo: carros[i].modelo,
+                cor: carros[i].cor,
+                placa: carros[i].placa
+            })
+        }
+
+        res.json(json)
+    },
+
+
+    buscarUm: async (req, res) => {
+
+        let json = { error: '', result: {} };
+
+        let codigo = req.params.codigo;
+        let carro = await CarroService.buscarUm(codigo);
+
+        if (carro) {
+            json.result = carro;
+        }
+
+        res.json(json);
+    },
+
+
+    cadastrarCarro: async (req, res) => {
+
+        let json = { error: '', result: {} };
+
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+        let cor = req.body.cor;
+
+        if (modelo && placa && cor) {
+            let carroCodigo = await CarroService.cadastrarCarro(modelo, placa, cor);
+            json.result = {
+                codigo: carroCodigo,
+                modelo,
+                placa,
+                cor
+            }
+        } else {
+            json.error = 'Campos não enviados'
+        }
+
+        res.json(json);
+    }
+
+
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
 const CarroController = require('./controllers/CarroController');
+
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carro/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.cadastrarCarro);
 
 module.exports = router;
