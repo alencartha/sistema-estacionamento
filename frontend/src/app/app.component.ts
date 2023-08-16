96b0ece93d5f3dfa27a927a2a39341d8418966e3
alencartha <alencar_tha@hotmail.com>
2023-08-15 21:16:25 -0300

    feature: front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
 
+
+interface Carro {
+  codigo?: number;
+  modelo: string;
+  cor: string;
+  placa: string;
+}
+
+const cars: Carro[] = [
+  {
+    codigo: 1,
+    modelo: 'Toyota Corolla',
+    cor: 'Azul',
+    placa: 'ABC123',
+  },
+  {
+    codigo: 2,
+    modelo: 'Honda Civic',
+    cor: 'Prata',
+    placa: 'DEF456',
+  },
+  {
+    codigo: 3,
+    modelo: 'Ford Mustang',
+    cor: 'Vermelho',
+    placa: 'GHI789',
+  },
+  {
+    codigo:4,
+    modelo: 'Volkswagen Golf',
+    cor: 'Branco',
+    placa: 'JKL012',
+  },
+  {
+    modelo: 'Chevrolet Camaro',
+    cor: 'Preto',
+    placa: 'MNO345',
+  },
+  // ... adicione mais carros se necessário
+];
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sistema-estacionamento';
+  title = 'Sistema Estacionamento';
+
+  page = 1;
+  pageSize = 4;
+  collectionSize = cars.length;
+  cars?: Carro[];
+
+  constructor() {
+    this.refreshCars();
+  }
+
+  refreshCars() {
+    this.cars = cars.map((country, i) => ({ id: i + 1, ...country })).slice(
+      (this.page - 1) * this.pageSize,
+      (this.page - 1) * this.pageSize + this.pageSize,
+    );
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule, NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { DecimalPipe, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    DecimalPipe, NgFor, FormsModule, NgbTypeaheadModule,
+     NgbPaginationModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
